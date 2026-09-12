de4e3333228c744d6eb81c5999c6704c8fc23532
Monirul Hasan Mithu <monirul.hasan513@gmail.com>
2026-09-13 03:09:30 +0600

    initial commit

---
+++ clearDay.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int r;
+    int c;
+    cin>> r;
+    cin>> c;
+    cout<<(7-(r+c));
+
+}
---
+++ luckySeven.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string S;
+    for(int i =0; i<=10; i++){
+        cin>>S[i];
+    }
+        cout<<S[6]; 
+    
+}
